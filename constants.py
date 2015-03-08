0d92f93fa5120538b0bbed7e27b9b69efd70eaea
bjc <wcember@gmail.com>
2015-03-08 17:02:39 -0400

    added templates and constants.py

---
+++ constants.py
@@ -0,0 +1,10 @@
+#global constants
+import os.path
+
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+TEST_DIR = os.path.join(BASE_DIR, 'test_folder')
+EPUB_TEMPLATES_DIR = os.path.join(BASE_DIR, 'epub_templates')
+
+
+
+
