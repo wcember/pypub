c22c8b1210dc6f0a4c058bd00db82bdfcfda5aa7
bjc <wcember@gmail.com>
2015-03-28 19:07:27 -0400

    removed functions no longer needed

--- epub_clean/clean.py
+++ epub_clean/clean.py
@@ -6,52 +6,6 @@ import lxml.html
 
 import constants
 
-def clean(input_unicode_string,
-        tag_dictionary=constants.SUPPORTED_TAGS):
-    '''Sanitizes HTML.
-
-    Sanitizes HTML. Tags not contained as keys in the tag_dictionary input are
-    removed, and child nodes are recursively moved to parent of removed node.
-    Attributes not contained as arguments in tag_dictionary are removed.
-
-    Args:
-        input_unicode_string: A unicode string representing HTML.
-        tag_dictionary: A dictionary with tags as keys and attributes as
-            values.
-
-    Returns:
-        A unicode string representing HTML. Doctype is set to <!DOCTYPE html>.
-
-    Raises:
-        TypeError: Raised if input_unicode_string isn't of type unicode.
-    '''
-    try:
-        assert type(input_unicode_string) == unicode
-    except AssertionError:
-        raise TypeError
-    node = lxml.html.fromstring(input_unicode_string)
-    stack = node.getchildren()
-    while stack:
-        current_node = stack.pop()
-        child_node_list = current_node.getchildren()
-        if not current_node.tag in tag_dictionary.keys():
-            parent_node = current_node.getparent()
-            parent_node.remove(current_node)
-            for n in child_node_list:
-                parent_node.insert(-1, n)
-        else:
-            attribute_dict = current_node.attrib
-            for attribute in current_node.attrib.keys():
-                if attribute not in tag_dictionary[current_node.tag]:
-                    attribute_dict.pop(attribute)
-        stack.extend(child_node_list)
-    unformatted_html_unicode_string = lxml.html.tostring(node,
-            pretty_print=True,
-            doctype='<!DOCTYPE html>',
-            encoding='unicode')
-    return unformatted_html_unicode_string
-
-
 def get_content(input_unicode_string, base_url):
     '''Extracts text and image links from HTML unicode string.
 
@@ -106,39 +60,6 @@ def get_content(input_unicode_string, base_url):
             content_list.append(('img', image_url))
     return content_list
 
-
-def condense(input_unicode_string):
-    try:
-        assert type(input_unicode_string) == unicode
-    except AssertionError:
-        raise TypeError
-    return re.sub('>\s+<','><',input_unicode_string).strip()
-
-def html_to_xhtml(html_unicode_string):
-    try:
-        assert type(html_unicode_string) == unicode
-    except AssertionError:
-        raise TypeError
-    node = lxml.html.fromstring(html_unicode_string)
-    #Confirm root node is html
-    try:
-        assert node.tag.lower() == 'html'
-    except AssertionError:
-        raise ValueError(''.join('html_unicode_string cannot be a fragment.',
-                'Root node tag is %s', node.tag))
-    #Add xmlns attribute to html node
-    node.set('xmlns', 'http://www.w3.org/1999/xhtml')
-    #Set DOCTYPE
-    DOCTYPE_string = constants.xhtml_doctype_string
-    string_with_open_singletons = lxml.etree.tostring(node, pretty_print=True,
-            encoding='unicode', doctype=DOCTYPE_string)
-    xhtml_unicode_string = string_with_open_singletons
-    #close singleton tag_dictionary
-    for tag in constants.SINGLETON_TAG_LIST:
-        xhtml_unicode_string = xhtml_unicode_string.replace(
-                '<' + tag + '/>',
-                '<' + tag + ' />')
-    return xhtml_unicode_string
 
 def validate(xhtml_unicode_string):
     parser = lxml.etree.XMLParser(dtd_validation=True)
--- epub_clean/unit_tests_clean.py
+++ epub_clean/unit_tests_clean.py
@@ -2,102 +2,11 @@ import re
 
 import unittest
 
-from clean import clean, condense, html_to_xhtml, get_content
+import clean
 
 class CleanTests(unittest.TestCase):
     def setUp(self):
         self.maxDiff = None
-    def test_condense(self):
-        pass
-    def test_clean_tags_full_html(self):
-        s = u'''
-                <!DOCTYPE html>
-                <html>
-                 <head>
-                 </head>
-                 <body>
-                  <div>Hello</div>
-                 </body>
-                </html>
-                '''
-        s1 = u'''
-                <!DOCTYPE html>
-                <html>
-                 <head>
-                 </head>
-                 <body>
-                  <div>Hello</div>
-                  <script>Uh oh...it's an evil script!</script>
-                 </body>
-                </html>
-                '''
-        s2 = u'''
-                <!DOCTYPE html>
-                <html>
-                 <head>
-                 </head>
-                 <body>
-                  <div>Hello</div>
-                 </body>
-                 <script>Uh oh...it's an evil script again!</script>
-                </html>
-                '''
-        s3 = u'''
-                <!DOCTYPE html>
-                <html>
-                 <head>
-                 </head>
-                 <body>
-                  <div>Hello</div>
-                 </body>
-                 <video>Play me!</video>
-                </html>
-                '''
-        s4 = u'''
-                <!DOCTYPE html>
-                <html>
-                 <head>
-                 </head>
-                 <body>
-                  <video>
-                   <div>Hello</div>
-                  </video>
-                 </body>
-                 <video>Play me!</video>
-                </html>
-                '''
-        self.assertEqual(condense(clean(s)), condense(s))
-        self.assertEqual(condense(clean(s1)), condense(s))
-        self.assertEqual(condense(clean(s2)), condense(s))
-        self.assertEqual(condense(clean(s3)), condense(s))
-        self.assertEqual(condense(clean(s4)), condense(s))
-
-    def test_html_to_xhtml(self):
-        s = u'''
-                <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-                <html xmlns="http://www.w3.org/1999/xhtml">
-                 <head>
-                 </head>
-                 <body>
-                  <div id="Test">Hello</div>
-                  <br />
-                  <br />
-                 </body>
-                </html>
-                '''
-        s1 = u'''
-                <!DOCTYPE html>
-                <html>
-                 <head>
-                 </head>
-                 <body>
-                  <DIV ID="Test">Hello</div>
-                  <br>
-                  <br>
-                 </body>
-                </html>
-                '''
-        self.assertEqual(condense(html_to_xhtml(clean((s1)))), condense(s))
 
     def test_get_content(self):
         s = u'''
@@ -112,7 +21,7 @@ class CleanTests(unittest.TestCase):
                  </body>
                 </html>
                 '''
-        self.assertEqual(get_content(s, u''), [
+        self.assertEqual(clean.get_content(s, u''), [
                 (u'div', u'Hello'),
         ])
 
