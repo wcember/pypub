1c5e363ac15d72ca5b564ef00503f8908b2e3bbd
bjc <wcember@gmail.com>
2016-01-17 19:09:50 -0500

    removed newer napoleon extension for compatibility with readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,8 +32,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.coverage',
-    'sphinxcontrib.napoleon',
-    'sphinx.ext.napoleon',]
+    'sphinxcontrib.napoleon',]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
