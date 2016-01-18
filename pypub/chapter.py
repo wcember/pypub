20ebb4b2906c060f8bcd44b8bb034b003edaebf5
bjc <wcember@gmail.com>
2016-01-18 18:46:52 -0500

    documentation

--- pypub/chapter.py
+++ pypub/chapter.py
@@ -9,7 +9,8 @@ import constants
 
 
 class Chapter(object):
-    '''Class representing an ebook chapter. By and large this shouldn't be
+    '''
+    Class representing an ebook chapter. By and large this shouldn't be
     called directly but rather one should use the class ChapterFactor to
     instantiate a chapter.
 
@@ -66,8 +67,9 @@ class Chapter(object):
 
 
 class ChapterFactory(object):
-    '''Used to create Chapter objects.Chapter objects can be created from urls,
-        files, and strings.
+    '''
+    Used to create Chapter objects.Chapter objects can be created from urls,
+    files, and strings.
 
     Args:
         clean_function (Option[function]): A function used to sanitize raw
@@ -81,10 +83,11 @@ class ChapterFactory(object):
         self.request_headers = {'User-Agent': user_agent}
 
     def create_chapter_from_url(self, url, title=None):
-        '''Creates a Chapter function from a url. Pulls the webpage from the
-            given url, sanitizes if using the clean_function method, and saves
-            it as the content of the created chapter.Basic webpage loaded
-            before any javascript executed.
+        '''
+        Creates a Chapter object from a url. Pulls the webpage from the
+        given url, sanitizes it using the clean_function method, and saves
+        it as the content of the created chapter. Basic webpage loaded
+        before any javascript executed.
 
         Args:
             url (string): The url to pull the content of the created Chapter
@@ -102,11 +105,45 @@ class ChapterFactory(object):
         return self.create_chapter_from_string(unicode_string, url, title)
 
     def create_chapter_from_file(self, file_name, url=None, title=None):
+        '''
+        Creates a Chapter object from an html or xhtml file. Sanitizes the
+        file's content using the clean_function method, and saves
+        it as the content of the created chapter.
+
+        Args:
+            file_name (string): The file_name containing the html or xhtml
+                content of the created Chapter
+            url (Option[string]): A url to infer the title of the chapter from
+            title (Option[string]): The title of the created Chapter. By
+                default, this is None, in which case the title will try to be
+                inferred from the webpage at the url.
+
+        Returns:
+            Chapter: A chapter object whose content is the given file
+                and whose title is that provided or inferred from the url
+        '''
         with codecs.open(file_name, 'r', encoding='utf-8') as f:
             content_string = f.read()
         return self.create_chapter_from_string(content_string, url, title)
 
     def create_chapter_from_string(self, html_string, url=None, title=None):
+        '''
+        Creates a Chapter object from a string. Sanitizes the
+        string using the clean_function method, and saves
+        it as the content of the created chapter.
+
+        Args:
+            html_string (string): The html or xhtml content of the created
+                Chapter
+            url (Option[string]): A url to infer the title of the chapter from
+            title (Option[string]): The title of the created Chapter. By
+                default, this is None, in which case the title will try to be
+                inferred from the webpage at the url.
+
+        Returns:
+            Chapter: A chapter object whose content is the given string
+                and whose title is that provided or inferred from the url
+        '''
         clean_html_string = self.clean_function(html_string)
         clean_xhtml_string = clean.html_to_xhtml(clean_html_string)
         if title:
