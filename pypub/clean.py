93ef133e0c781b9171940d5f4a343c35a6ea087f
bjc <wcember@gmail.com>
2016-05-16 22:20:48 -0400

    pep8

--- pypub/clean.py
+++ pypub/clean.py
@@ -1,4 +1,3 @@
-import cgi
 import re
 
 from bs4 import BeautifulSoup
@@ -6,8 +5,9 @@ from bs4.dammit import EntitySubstitution
 
 import constants
 
+
 def clean(input_string,
-        tag_dictionary=constants.SUPPORTED_TAGS):
+          tag_dictionary=constants.SUPPORTED_TAGS):
     """
     Sanitizes HTML. Tags not contained as keys in the tag_dictionary input are
     removed, and child nodes are recursively moved to parent of removed node.
@@ -37,7 +37,7 @@ def clean(input_string,
     while stack:
         current_node = stack.pop()
         child_node_list = current_node.findAll(True, recursive=False)
-        if not current_node.name in tag_dictionary.keys():
+        if current_node.name not in tag_dictionary.keys():
             parent_node = current_node.parent
             current_node.extract()
             for n in child_node_list:
@@ -48,7 +48,7 @@ def clean(input_string,
                 if attribute not in tag_dictionary[current_node.name]:
                     attribute_dict.pop(attribute)
         stack.extend(child_node_list)
-    #Remove img tags without src attribute
+    # Remove img tags without src attribute
     image_node_list = root.find_all('img')
     for node in image_node_list:
         if not node.has_attr('src'):
@@ -56,14 +56,16 @@ def clean(input_string,
     unformatted_html_unicode_string = unicode(root.prettify(encoding='utf-8',
                                                             formatter=EntitySubstitution.substitute_html),
                                               encoding='utf-8')
-    #fix <br> tags since not handled well by default by bs4
+    # fix <br> tags since not handled well by default by bs4
     unformatted_html_unicode_string = unformatted_html_unicode_string.replace('<br>', '<br/>')
-    #remove &nbsp; and replace with space since not handled well by certain e-readers
+    # remove &nbsp; and replace with space since not handled well by certain e-readers
     unformatted_html_unicode_string = unformatted_html_unicode_string.replace('&nbsp;', ' ')
     return unformatted_html_unicode_string
 
+
 def condense(input_string):
-    '''Trims leadings and trailing whitespace between tags in an html document
+    """
+    Trims leadings and trailing whitespace between tags in an html document
 
     Args:
         input_string: A (possible unicode) string representing HTML.
@@ -73,42 +75,44 @@ def condense(input_string):
 
     Raises:
         TypeError: Raised if input_string isn't a unicode string or string.
-    '''
+    """
     try:
         assert isinstance(input_string, basestring)
     except AssertionError:
         raise TypeError
-    removed_leading_whitespace = re.sub('>\s+','>', input_string).strip()
-    removed_trailing_whitespace = re.sub('\s+<','<', removed_leading_whitespace).strip()  #Need to fix this so replaces whitespace with words between tags
+    removed_leading_whitespace = re.sub('>\s+', '>', input_string).strip()
+    removed_trailing_whitespace = re.sub('\s+<', '<', removed_leading_whitespace).strip()
     return removed_trailing_whitespace
 
+
 def html_to_xhtml(html_unicode_string):
-    '''Converts html to xhtml
+    """
+    Converts html to xhtml
 
     Args:
-        input_string: A (possible unicode) string representing HTML.
+        html_unicode_string: A (possible unicode) string representing HTML.
 
     Returns:
         A (possibly unicode) string representing XHTML.
 
     Raises:
         TypeError: Raised if input_string isn't a unicode string or string.
-    '''
+    """
     try:
         assert isinstance(html_unicode_string, basestring)
     except AssertionError:
         raise TypeError
     root = BeautifulSoup(html_unicode_string, 'html.parser')
-    #Confirm root node is html
+    # Confirm root node is html
     try:
         assert root.html is not None
     except AssertionError:
         raise ValueError(''.join(['html_unicode_string cannot be a fragment.',
-                'string is the following: %s', unicode(root)]))
-    #Add xmlns attribute to html node
+                         'string is the following: %s', unicode(root)]))
+    # Add xmlns attribute to html node
     root.html['xmlns'] = 'http://www.w3.org/1999/xhtml'
-    unicode_string = unicode(root.prettify(encoding='utf-8', formatter='html'), encoding = 'utf-8')
-    #close singleton tag_dictionary
+    unicode_string = unicode(root.prettify(encoding='utf-8', formatter='html'), encoding='utf-8')
+    # Close singleton tag_dictionary
     for tag in constants.SINGLETON_TAG_LIST:
         unicode_string = unicode_string.replace(
                 '<' + tag + '/>',
