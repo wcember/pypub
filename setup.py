73d7b2ab60fb124ae15a4de59df85a5d63a5c9e8
bjc <wcember@gmail.com>
2016-05-21 17:46:21 -0400

    version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='pypub',
-    version='1.1',
+    version='1.2',
     packages=['pypub',],
     package_data={'pypub': ['epub_templates/*',]},
     author = 'William Cember',
