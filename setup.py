28562b562dcde0e877aafe2e7129aa30b74bd338
imgurbot12 <imgurbot12@gmail.com>
2022-11-13 16:33:55 -0700

    chore: upgrade version after readme PR

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name='pypub3',
-    version='1.0.0',
+    version='1.0.1',
     license='MIT',
     author='Andrew Scott',
     author_email='imgurbot12@gmail.com',
